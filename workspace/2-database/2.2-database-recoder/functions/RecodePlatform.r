d83317140234693d5b62c95c6a28f758c7a6e331
Renato Dinhani <renatodinhani@gmail.com>
2018-05-08 03:14:09 -0300

    Recode

--- workspace/2-database/2.2-database-recoder/functions/RecodePlatform.r
+++ workspace/2-database/2.2-database-recoder/functions/RecodePlatform.r
@@ -1,5 +1,24 @@
 RecodePlatform = function(v){
     Recode(v,
-        "PlayStation 4 Pro" = "PlayStation 4"
+        # PlayStation
+        "PlayStation VR" = "PlayStation 4",
+        "PlayStation 4 Pro" = "PlayStation 4",
+
+        # Windows
+        "x86"  = "Windows",
+        "Microsoft Windows" = "Windows",
+        "Windows 3.x" = "Windows",
+        "Windows 3.1" = "Windows",
+        "Windows 95" = "Windows",
+        "Windows 98" = "Windows",
+        "Windows 9x" = "Windows",
+        "Windows 2000" = "Windows",
+        "Windows Me" = "Windows",
+        "Windows XP" = "Windows",
+        "Windows Vista"  = "Windows",
+        "Windows 7"  = "Windows",
+        "Windows 8"  = "Windows",
+        "Windows 8.1"  = "Windows",
+        "Windows 10" = "Windows"
     )
 }
