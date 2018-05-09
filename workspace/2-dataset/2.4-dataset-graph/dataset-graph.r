8eec1ea09269adf161742efdf6dfe9fd40cab110
Renato Dinhani <renatodinhani@gmail.com>
2018-05-08 22:42:00 -0300

    Graph generator

--- workspace/2-dataset/2.4-dataset-graph/dataset-graph.r
+++ workspace/2-dataset/2.4-dataset-graph/dataset-graph.r
@@ -12,9 +12,52 @@ games = readRDS("../2.2-dataset-recoder/data/games.rds")
 # ==============================================================================
 # GENERATE GRAPH
 # ==============================================================================
-g = games %>%
-  gather(Attribute, Value, -c(ID, Name)) %>%
-  filter(sapply(Value, negate(is.null))) %>%
+# DF
+graph.df = games %>%
+  gather(AttrKey, AttrValue,-c(ID, Name)) %>%
+  filter(sapply(AttrValue, negate(is.null))) %>%
   unnest() %>%
-  select(Name, Value) %>%
-  graph_from_data_frame(directed = FALSE)
+  mutate(
+    ID = paste("Game", Name),
+    AttrID = paste(AttrKey, AttrValue)
+  )
+  
+# VERTICES
+graph.vertices1 = graph.df %>%
+  select(ID, Name) %>%
+  rename(
+    Label = Name
+  ) %>%
+  mutate(
+    Type = "Game"
+  ) %>%
+  select(ID, Label, Type)
+
+graph.vertices2 = graph.df %>%
+  select(AttrID, AttrKey, AttrValue) %>%
+  distinct() %>%
+  rename(
+    ID  = AttrID,
+    Label = AttrValue,
+    Type = AttrKey
+  ) %>%
+  select(ID, Label, Type)
+
+graph.vertices = graph.vertices1 %>% 
+  union_all(graph.vertices2) %>%
+  distinct()
+
+# EDGES
+graph.edges = graph.df %>%
+  select(ID, AttrID) %>%
+  mutate(
+    Weight = 1
+  )
+
+# GRAPH
+graph = graph_from_data_frame(graph.edges, vertices = graph.vertices)
+
+# ==============================================================================
+# SAVE GRAPH
+# ==============================================================================
+saveRDS(graph, file = "data/graph.rds")
