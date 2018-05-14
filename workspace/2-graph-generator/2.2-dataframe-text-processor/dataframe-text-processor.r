b2bdf0715eb71dc41c9df10e3e4baf653a97f5e9
Renato Dinhani <renatodinhani@gmail.com>
2018-05-13 22:51:20 -0300

    Text Processor

--- workspace/2-graph-generator/2.2-dataframe-text-processor/dataframe-text-processor.r
+++ workspace/2-graph-generator/2.2-dataframe-text-processor/dataframe-text-processor.r
@@ -12,8 +12,12 @@ games <- readRDS("../2.1-dataframe-joiner/data/games.rds")
 # ==============================================================================
 # PROCESS GAMES TEXTS
 # ==============================================================================
+games.corpus = games$Texts$Description %>% VectorSource() %>% VCorpus()
+games.corpus = games.corpus %>% tm_map(removePunctuation) %>% tm_map(content_transformer(iconv))
+games.tdm = TermDocumentMatrix(games.corpus)
 
 # ==============================================================================
 # SAVE GAMES
 # ==============================================================================
+games$TDM = games.tdm
 saveRDS(games, file = "data/games.rds")
