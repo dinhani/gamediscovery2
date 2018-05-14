7b1260f58a91a75cb797706c28dc1ba191894794
Renato Dinhani <renatodinhani@gmail.com>
2018-05-13 23:59:38 -0300

    Testing identification of characters

--- workspace/2-graph-generator/2.4-dataframe-enhancer/functions/IdentifyCharacter.r
+++ workspace/2-graph-generator/2.4-dataframe-enhancer/functions/IdentifyCharacter.r
@@ -2,11 +2,19 @@
 # Identify characters of the existing games
 # ==============================================================================
 IdentifyCharacter <- function(games.texts, games.tdm) {
-  # CHARACTERS
   characters <- c()
-  characters <- c(characters, "Goku", "Vegeta")
-  characters <- c(characters, "Batman", "Captain America", "Catwoman", "Deadpool", "Hulk", "Iron Man", "Joker", "Robin", "Spider-Man", "Superman", "Venom", "Wolverine")
-  characters <- c(characters, "Hercules", "Thor", "Zeus")
+  # Anime
+  characters <- c(characters, "Gohan", "Goku", "Vegeta")
+  # Disney
+  characters <- c(characters, "Donald Duck", "Mickey", "Pinocchio")
+  # Games
+  characters <- c(characters, "Gordon Freeman", "Kratos", "Lara Croft", "Master Chief", "Pac Man", "Sonic", "Yoshi")
+  # Gods
+  characters <- c(characters, "Anubis", "Aphrodite", "Apollo", "Ares", "Artemis", "Athena", "Cronos", "Hades", "Helios", "Hercules", "Odin", "Osiris", "Poseidon", "Thor", "Xena", "Zeus")
+  # Historical
+  characters <- c(characters, "Archimedes", "Einstein",  "Hitler", "Julius Caesar", "Leonidas", "Socrates", "Tesla")
+  # Marvel / DC
+  characters <- c(characters, "Batman", "Captain America", "Catwoman", "Deadpool", "Hulk", "Iron Man", "Joker", "Robin", "Spider-Man", "Superman", "Thanos", "Venom", "Wolverine")
 
   # IDENTIFY
   IdentifyValues(games.texts, games.tdm, "Character", characters)
