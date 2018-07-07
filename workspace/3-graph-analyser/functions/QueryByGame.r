14a2b799a3d44fe7a5bca8b24855fdec770c1ab4
Renato Dinhani <renatodinhani@gmail.com>
2018-07-06 23:56:33 -0300

    Improving readability of QueryByGame

--- workspace/3-graph-analyser/functions/QueryByGame.r
+++ workspace/3-graph-analyser/functions/QueryByGame.r
@@ -5,24 +5,24 @@ QueryByGame <- function(g, g.es, game) {
 
   # get features connected to the requested game
   features.vertices <- adjacent_vertices(g, game)[[1]]
-  features.types <- features.vertices$Type
-  features.types.table <- table(features.types)
+  features.vertices.types <- features.vertices$Type
+  features.vertices.types.table <- table(features.vertices.types)
 
   # get edges between the found features and other games
-  features.edges <- g.es[inc(features.vertices)]
-  features.edges.features <- head_of(g, features.edges)
-  features.edges.games <- tail_of(g, features.edges)
-  features.edges.weights <- edge_attr(g, "Weight", features.edges)
+  features.vertices.edges <- g.es[inc(features.vertices)]
+  features.vertices.edges.feature <- head_of(g, features.vertices.edges)
+  features.vertices.edges.game <- tail_of(g, features.vertices.edges)
+  features.vertices.edges.weight <- edge_attr(g, "Weight", features.vertices.edges)
 
   # generate a table to perform calculations
   games.df1 <- data.table(
-    Game = features.edges.games$name,
-    FeatureType = features.edges.features$Type,
-    FeatureWeight = features.edges.weights
+    Game = features.vertices.edges.game$name,
+    FeatureType = features.vertices.edges.feature$Type,
+    FeatureWeight = features.vertices.edges.weight
   )
 
   # calculate type intersection with main game
-  games.df2 <- games.df1[, .(FreqIntersection = mean(FeatureWeight) * (.N / features.types.table[FeatureType])), by = .(Game, FeatureType)]
+  games.df2 <- games.df1[, .(FreqIntersection = mean(FeatureWeight) * (.N / features.vertices.types.table[FeatureType])), by = .(Game, FeatureType)]
 
   # calculate intersection of each game with main game
   games.df3 <- games.df2[, .(FreqIntersectionTotal = sum(FreqIntersection)), by = .(Game)]
