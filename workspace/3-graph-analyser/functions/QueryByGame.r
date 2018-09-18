9ffa11d91cb2b98c18cb8a7312029e9f53472a92
Renato Dinhani <renatodinhani@gmail.com>
2018-09-18 00:51:53 -0300

    Improving code readability

--- workspace/3-graph-analyser/functions/QueryByGame.r
+++ workspace/3-graph-analyser/functions/QueryByGame.r
@@ -3,34 +3,34 @@
 # ==============================================================================
 QueryByGame <- function(g, g.es, game) {
 
-  # get features connected to the requested game
+  # get features connected to the searched game
   features.vertices <- adjacent_vertices(g, game)[[1]]
   features.vertices.types <- features.vertices$Type
   features.vertices.types.table <- table(features.vertices.types)
 
-  # get edges between the found features and other games
+  # get edges between the features of the searched game and other games
   features.edges <- g.es[inc(features.vertices)]
   features.edges.feature <- head_of(g, features.edges)
   features.edges.game <- tail_of(g, features.edges)
   features.edges.weight <- edge_attr(g, "Weight", features.edges)
 
-  # generate a table to perform calculations
+  # generate a table of related games with their features
   games.df1 <- data.table(
     Game = features.edges.game,
     FeatureType = features.edges.feature$Type,
     Weight = features.edges.weight
   )
 
-  # calculate type Weight with main game
+  # calculate weight intersection of features of related games with main game
   games.df2 <- games.df1[, .(FeatureCount = .N, Weight = mean(Weight)), by = .(Game, FeatureType)]
   games.df2$MainGameFeatureCount <- features.vertices.types.table[games.df2$FeatureType]
   games.df2$FeatureIntersection <- games.df2$FeatureCount / games.df2$MainGameFeatureCount
-  games.df2$Weight <- games.df2$Weight * games.df2$FeatureIntersection
+  games.df2$WeightIntersection <- games.df2$Weight * games.df2$FeatureIntersection
 
-  # calculate Weight of each game with main game
-  games.df3 <- games.df2[, .(Weight = sum(Weight)), by = .(Game)]
+  # calculate Weight of each game with main game using all the features
+  games.df3 <- games.df2[, .(WeightIntersection = sum(WeightIntersection)), by = .(Game)]
 
-  # get games games ordered by best Weights and extract
-  games.ids <- games.df3[order(Weight, decreasing = TRUE), Game, ]
+  # get games games ordered by best weights and extract
+  games.ids <- games.df3[order(WeightIntersection, decreasing = TRUE), Game, ]
   V(g)[games.ids]
 }
