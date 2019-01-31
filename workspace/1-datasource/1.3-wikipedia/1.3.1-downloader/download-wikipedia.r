2252b589e40e394d1548d4952d8fc632db491eda
Renato Dinhani <renatodinhani@gmail.com>
2019-01-31 19:43:49 -0200

    Additional check for Wikipedia download

--- workspace/1-datasource/1.3-wikipedia/1.3.1-downloader/download-wikipedia.r
+++ workspace/1-datasource/1.3-wikipedia/1.3.1-downloader/download-wikipedia.r
@@ -14,7 +14,7 @@ games <- readRDS("1-datasource//1.1-wikidata/1.1.2-parser/data/games.rds")
 plan(multiprocess, workers = 2)
 future_walk2(games$WD_GameID, games$WD_GameLink, .progress = TRUE, .f = safely(function(game.id, game.url) {
   # do not download if without link
-  if (length(game.url) == 0) {
+  if (length(game.url) == 0 || game.url == "") {
     return(NULL)
   }
 
